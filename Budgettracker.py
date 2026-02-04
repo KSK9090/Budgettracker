ba8c8144944a93187ec739d973afa34dcfc7d4f9
Daa <Daa@BA01-001-VI04.Bayern.daa>
2026-02-04 13:45:37 +0100

    Match Variante hinzugefügt und Code verbessert, betrag war falsch gesetzt

--- Budgettracker.py
+++ Budgettracker.py
@@ -1,21 +1,23 @@
 stand = 0
 
 while True:
-    """Schleife damit das Programm nicht frühzeitig beendet"""
+    # Schleife, damit das Programm nicht frühzeitig beendet wird
     print("\n1=Hinzufügen, 2=Kontostand, 3=Beenden")
     auswahl = input("Option: ").strip()
 
     if auswahl == "1":
         typ = input("Typ (Einnahme/Ausgabe): ").strip()
-        betrag = float(input("Betrag: "))
-        if typ == "Einnahme":
-            stand += betrag
-        
-        elif typ == "Ausgabe":
-            stand -= betrag
-        else:
-            print("Bitte wählen Sie nur zwischen 'Eingabe' und 'Ausgabe'.")
 
+        if typ == "Einnahme":
+            betrag = float(input("Betrag: "))
+            stand += betrag
+
+        elif typ == "Ausgabe":
+            betrag = float(input("Betrag: "))
+            stand -= betrag
+
+        else:
+            print("Bitte wählen Sie nur zwischen 'Einnahme' und 'Ausgabe'.")
 
     elif auswahl == "2":
         print(f"Kontostand: {stand}")
@@ -24,6 +26,35 @@ while True:
         break
 
     else:
-        print("Das ist keine gültige Eingabe. Bitte versuchen Sie es erneut unter Einbehaltung des Menüs.")
+        print("Das ist keine gültige Eingabe. Bitte versuchen Sie es erneut.")
 
 
+"""
+while True:
+    print("\n1=Hinzufügen, 2=Kontostand, 3=Beenden")
+    auswahl = input("Option: ").strip()
+
+    match auswahl:
+        case "1":
+            typ = input("Typ (Einnahme/Ausgabe): ").strip()
+            
+            if typ == "Einnahme":
+                betrag = float(input("Betrag: "))
+                stand += betrag
+        
+            elif typ == "Ausgabe":
+                betrag = float(input("Betrag: "))
+                stand -= betrag
+
+            else:
+                print("Bitte wählen Sie nur zwischen 'Einnahme' und 'Ausgabe'.")
+
+        case "2":
+            print(f"Kontostand: {stand}")
+    
+        case "3":
+            break
+
+        case _:
+            print("Ihre Eingabe ist ungültig. Bitte versuchen Sie es erneut.")
+"""
